6f37de24f1bd220b6abc551814755e60018a42dc
Bradley Monk <brad.monk@gmail.com>
2016-07-14 00:06:00 -0700

    preview stack now accepts timeline info

--- grinsubfunctions/previewstack.m
+++ grinsubfunctions/previewstack.m
@@ -1,24 +1,43 @@
-function [] = previewstack(IMGS)
+function [] = previewstack(IM, varargin)
+
+keyboard
+
+if nargin > 1
+    fh1=figure('Units','normalized','OuterPosition',[.05 .05 .9 .7],'Color','w');
+    hax1 = axes('Position',[.05 .05 .45 .9],'Color','none','XTick',[],'YTick',[]);
+    hax2 = axes('Position',[.55 .05 .45 .9],'Color','none','XTick',[],'YTick',[]);
+    
+    
+    
+    
+    
+else
+    
+    fh1=figure('Units','normalized','OuterPosition',[.05 .05 .6 .8],'Color','w');
+    hax1 = axes('Position',[.05 .05 .9 .9],'Color','none','XTick',[],'YTick',[]);
+    
+    
+end
 
 
-fh1=figure('Units','normalized','OuterPosition',[.05 .05 .6 .7],'Color','w');
-hax1 = axes('Position',[.05 .05 .9 .9],'Color','none','XTick',[],'YTick',[]);
 
-imagesc(IMGS(:,:,1))
+
+
+ih = imagesc(IM(:,:,1));
 colormap(jet)
 
-mx = max(max(IMGS(:,:,1)));
-mn = min(min(IMGS(:,:,1)));
+mx = max(max(IM(:,:,1))) * 1.2;
+mn = min(min(IM(:,:,1))) * 0.8;
 
-for nT = 1:length(IMGS)
-  
-    muuIMG = muIMG(:,:,nT);
+ndims = numel(size(IM));
 
-    imagesc(muuIMG)
+for nT = 1:size(IM,ndims)
+
+    ih.CData = IM(:,:,nT);
     
     pause(.05)
 end
-%----------------------
+
 
 
 end
