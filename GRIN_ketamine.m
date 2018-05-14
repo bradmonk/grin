802f150088ee441a6e7736bed321cda82403ede5
Bradley Monk <brad.monk@gmail.com>
2018-05-14 11:50:02 -0700

    Ketamine study instructions

---
+++ GRIN_ketamine.m
@@ -0,0 +1,58 @@
+%% GRIN ketamine
+%{
+
+we want to see if bursting changes with ketamine treatment, and if this
+is specific to the ?reward sensitive? (RS) cells. Steve will be sending
+us mouse IDs, session IDs and pics of his ROI, which I think we could
+match up to our aligned stacks.
+
+We could look for the bursting in the baseline periods of each trial.
+
+We?d like to know a) if bursting is specific to RS cells; b) if this is
+affected by introduction of shock trials; and c) if ketamine has an
+effect on this.
+
+For now, the data in the paper are from animals: 33 65 74 93. So we should
+focus getting these formatted the same, and analyze these.
+
+Yes, Chenyu, we need to get the Long sessions with injections aligned. We
+will look at bursting before and after injections.
+
+
+%}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
